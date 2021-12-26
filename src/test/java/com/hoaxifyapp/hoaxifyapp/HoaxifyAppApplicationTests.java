09888f48770d2d0318f89092e853a49bf78ddeb8
turgay-dede <turgay.dede28@hotmail.com>
2021-12-26 11:59:57 +0300

    initial commit

---
+++ src/test/java/com/hoaxifyapp/hoaxifyapp/HoaxifyAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoaxifyapp.hoaxifyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoaxifyAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
